8fd45ff745f13f824fb4a89048ee927757fe3ad3
eevitg <vketh@yandex.ru>
2023-11-24 11:08:56 +0300

    add card component structure

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import '../App.css';
+import React from 'react'
+
+function CardFront(props) {
+    return (
+        <div className="card">
+            <img className="img" src={props.url} alt="cardImg"/>
+            <p className="paragraph">{props.theme}</p>
+            <h4 className="wordMain">{props.word}</h4>
+            <h4 className="wordMain">{props.wordTranslation}</h4>
+            <p className="paragraph">{props.transcription}</p>
+            <button className="button">{props.flipButton}</button>
+            <button className="button">{props.changeButton}</button>
+            <button className="button">{props.deleteButton}</button>
+        </div>
+    );
+  }
+
+
+export default CardFront;
